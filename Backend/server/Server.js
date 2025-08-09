9ede880dfc39dde62c4e52e904e6d45e66cac5fe
shishirdubey24 <shishirdubey221@gmail.com>
2025-08-09 21:56:55 +0530

    Changed

--- Backend/server/Server.js
+++ Backend/server/Server.js
@@ -2,6 +2,7 @@ import express from "express";
 import ConnectDb from "../Models/Db.js";
 import bodyParser from "body-parser";
 import cors from "cors";
+import AuthRouter from "../Routes/AuthRouter.js";
 const app = express();
 
 ConnectDb();
@@ -12,6 +13,7 @@ app.get("/", (req, res) => {
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use("/auth", AuthRouter);
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
