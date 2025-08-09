288c5570fb0689700e8da5ca821553eb7674a356
shishirdubey24 <shishirdubey221@gmail.com>
2025-08-09 21:54:44 +0530

    Modified

--- Backend/Models/Db.js
+++ Backend/Models/Db.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-console.log("MONGO_URI from env:", process.env.MONGO_URL);
 
 const ConnectDb = async () => {
   try {
