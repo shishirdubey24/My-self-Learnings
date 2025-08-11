89eb33f4272940f2a736bb0dfd10f6294aeeda42
shishirdubey24 <shishirdubey221@gmail.com>
2025-08-11 20:07:50 +0530

    Chaged Structer

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,14 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Registration from "./routers/Registration.jsx"
 import { Provider } from 'react-redux'
 import store from './store/index.js'
+import {RouterProvider, createBrowserRouter} from "react-router-dom"
+
+const router=createBrowserRouter([
+ {
+   path:"/",
+   element:<App/>,
+   children:[
+   
+   {path:"/user", element:<Registration/> }, 
+  
+
+   ]
+ }
+])
 
 createRoot(document.getElementById('root')).render(
 
   <Provider store={store}>
+   <RouterProvider  router={router}/>
  <StrictMode>
-    <App />
+   
   </StrictMode>,
   </Provider>
  
