2c74c387bb51e87999faf3c0da1d9cbb89cf890d
shishirdubey24 <shishirdubey221@gmail.com>
2025-08-12 14:14:46 +0530

    Changed Imports

--- Backend/Middlewares/AuthValidation.js
+++ Backend/Middlewares/AuthValidation.js
@@ -1,11 +1,23 @@
 import Joi from "joi";
-const signupValidation = (req, res, next) => {
+export const signupValidation = (req, res, next) => {
   const schema = Joi.object({
     name: Joi.string().min(3).max(100).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(3).max(100).required(),
   });
-  const { error } = schema.validation(req.body);
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(404).json({ message: "Bad request", error });
+  }
+  next();
+};
+
+export const loginValidation = (req, res, next) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(3).max(100).required(),
+  });
+  const { error } = schema.validate(req.body);
   if (error) {
     return res.status(404).json({ message: "Bad request", error });
   }
