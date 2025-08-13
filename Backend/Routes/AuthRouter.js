ad7b29bc04c380a79babd320c700fa260713303f
shishirdubey24 <shishirdubey221@gmail.com>
2025-08-13 16:22:55 +0530

    Improved structer and syntax

--- Backend/Routes/AuthRouter.js
+++ Backend/Routes/AuthRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
+import { signupValidation } from "../Middlewares/AuthValidation.js";
+import { signup } from "../Controllers/Auth.js";
 const router = express.Router();
 
 router.post("/login", (req, res) => {
   res.send("Login SUcess");
 });
 
-router.post("/Signup", (req, res) => {
-  res.send("SignUp done!");
-});
+router.post("/Signup", signupValidation, signup);
+
 export default router;
