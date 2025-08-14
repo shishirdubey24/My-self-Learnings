bca73bdfe34bdb00ea86c9562d15a41ef3578628
shishirdubey24 <shishirdubey221@gmail.com>
2025-08-14 14:37:05 +0530

    Coomit

--- Backend/Controllers/Auth.js
+++ Backend/Controllers/Auth.js
@@ -1,4 +1,19 @@
+import bcrypt from "bcrypt";
+import UserModel from "../Models/User.js";
 export const signup = async (req, res) => {
   try {
-  } catch (error) {}
+    const { name, email, password } = req.body;
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      return res
+        .status(409)
+        .json({ message: "User is SignedIn", success: false });
+    }
+    const userModel = new UserModel({ name, email, password });
+    userModel.password = await bcrypt.hash(password, 10);
+    await userModel.save();
+    res.status(201).json({ message: "Done", success: true });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Error", success: false });
+  }
 };
