48cac5a88e89189ba6373d9686b33decba77f570
shishirdubey24 <shishirdubey221@gmail.com>
2025-08-17 21:23:22 +0530

    Added JWT Token and Login MOdule

--- Backend/Controllers/Auth.js
+++ Backend/Controllers/Auth.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import UserModel from "../Models/User.js";
+import jwt from "jsonwebtoken";
 export const signup = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -15,5 +16,48 @@ export const signup = async (req, res) => {
     res.status(201).json({ message: "Done", success: true });
   } catch (error) {
     res.status(500).json({ message: "Internal Error", success: false });
+  }
+};
+
+export const Login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(403).json({
+        message: "Auth Faield email or password is wrong",
+        success: false,
+      });
+    }
+    // CHeck Passowrd is correct or not
+    const isPasswordEql = await bcrypt.compare(password, user.password);
+    if (!isPasswordEql) {
+      return res.status(403).json({
+        message: "Auth Faield email or password is wrong",
+        success: false,
+      });
+    }
+    // create Jwt token
+
+    const jwToken = jwt.sign(
+      { email: user.email, id: user._id },
+      process.env.JWT_SECREAT,
+      { expiresIn: "24hr" }
+    );
+
+    //send response after token generation::
+
+    res.status(200).json({
+      message: "LogedIn",
+      success: true,
+      jwToken,
+      email,
+      name: user.name,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Error",
+      success: false,
+    });
   }
 };
