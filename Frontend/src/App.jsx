d5ec94f764fcbf32aa054c69ad864dfdc9493903
shishirdubey24 <shishirdubey221@gmail.com>
2025-08-18 12:55:49 +0530

    Changed the routing setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,15 @@
 
 import './App.css'
-import Fetchdata from './components/Fetchdata'
 import Header from './components/Header'
-import ProductDisplay from './components/ProductDisplay'
-//import ShimmerUI from './components/ShimmerUI'
 import { Outlet } from 'react-router'
+import Footer from './components/Footer'
 function App() {
-
 
   return (
   <>
       <Header/>
        <Outlet/>
-      <Fetchdata/>
-       <ProductDisplay/>
-     
+      <Footer/>
   </>
   )
 }
