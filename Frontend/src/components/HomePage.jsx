27505b775b28ebe46c58bf5ff81c457932a372e8
shishirdubey24 <shishirdubey221@gmail.com>
2025-08-19 14:58:43 +0530

    Added New File for better sttructer

---
+++ Frontend/src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import Fetchdata from "./Fetchdata"
+import ProductDisplay from "./ProductDisplay"
+
+const HomePage = () => {
+  return (
+    <div>
+      <Fetchdata/>
+      <ProductDisplay/>
+    </div>
+  )
+}
+
+export default HomePage
