1bb0a99a8681942f1b5bf8b95f4332526ee5826a
shishirdubey24 <shishirdubey221@gmail.com>
2025-10-03 18:31:48 +0530

    Token route

--- Backend/Routes/AuthRouter.js
+++ Backend/Routes/AuthRouter.js
@@ -4,9 +4,15 @@ import {
   signupValidation,
 } from "../Middlewares/AuthValidation.js";
 import { Login, signup } from "../Controllers/Auth.js";
+import { verifyToken } from "../Middlewares/JwtValidation.js";
 const router = express.Router();
 
-router.post("/Signup", signupValidation, signup);
-router.post("/Login", loginValidation, Login);
-
+router.post("/signup", signupValidation, signup);
+router.post("/login", loginValidation, Login);
+router.get("/user", verifyToken, (req, res) => {
+  res.json({
+    success: true,
+    user: { id: req.user.id, email: req.user.email, name: req.user.name },
+  });
+});
 export default router;
