59580a23d024232e81527947e666521a4c5f91e3
shishirdubey24 <shishirdubey221@gmail.com>
2025-10-03 18:33:50 +0530

    Commit

--- Backend/server/Server.js
+++ Backend/server/Server.js
@@ -1,17 +1,28 @@
 import express from "express";
 import ConnectDb from "../Models/Db.js";
-import bodyParser from "body-parser";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import AuthRouter from "../Routes/AuthRouter.js";
 const app = express();
-
 ConnectDb();
 app.get("/", (req, res) => {
   res.send("Hellow World!!");
 });
-
-app.use(bodyParser.json());
-app.use(cors());
+const corsOption = {
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:8080",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOption));
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/auth", AuthRouter);
 const PORT = 5000;
