b8095a4ba41439563eaa5a3f5c83f896f1f75be7
shishirdubey24 <shishirdubey221@gmail.com>
2026-02-04 18:41:39 +0530

    Updated main file

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,30 +3,32 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Registration from "./routers/Registration.jsx"
+import Login from './routers/Login.jsx'
+import Profile from './routers/Profile.jsx'
 import { Provider } from 'react-redux'
 import store from './store/index.js'
 import {RouterProvider, createBrowserRouter} from "react-router-dom"
-
+import HomePage from './components/HomePage.jsx'
 const router=createBrowserRouter([
  {
    path:"/",
    element:<App/>,
    children:[
-   
-   {path:"/user", element:<Registration/> }, 
-  
-
+    { 
+        index: true,           
+        element: <HomePage/>   
+      },
+   {path:"/signup", element:<Registration/> }, 
+   {path:"/login",element:<Login/>},
+    {path:"/profile",element:<Profile/>}
    ]
  }
 ])
 
 createRoot(document.getElementById('root')).render(
-
-  <Provider store={store}>
-   <RouterProvider  router={router}/>
- <StrictMode>
-   
-  </StrictMode>,
-  </Provider>
- 
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
+  </StrictMode>
 )
